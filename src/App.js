8871d7d1d5393e00565e54b35e017d29a8b2628b
Mayuri Rathod <mayuri.rathod@imaginea.com>
2019-09-06 19:42:39 +0530

    Changes for component approach

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
-const App = () => <h1>Default</h1>
+/* Class Approach
+  - This maintains state
+*/
+class App extends React.Component {
+  render() {
+    // return <h1>Hello JSX....!</h1>
+    return React.createElement('h1', null, 'Hello Create Element...!')
+  }
+}
+
+/* JSX approach
+  - This is stateless
+*/
+// const App = () => <h1>Hello, I am State Less </h1>
 
 export default App;
