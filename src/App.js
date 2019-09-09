54700a576f8a8b9603fb12655b073d879d1bda35
Mayuri Rathod <mayuri.rathod@imaginea.com>
2019-09-09 18:54:23 +0530

    Example to create custom propTypes validation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
 import React from 'react';
 
-const App = () => <h1>Default</h1>
+class App extends React.Component {
+  render() {
+    // 1. To check isRequired condition
+    // return <Title />
+    // 2. To check type condition 
+    // return <Title text={false} />
+    // 3. Correct conditions
+    return <Title text="Correct condition...!" />
+  }
+}
+
+const Title = (props) => <h1> Title: {props.text} </h1>
+
+Title.propTypes = {
+  // props - All the props
+  // propName - Name of the prop
+  // component - Component
+  text(props, propName, component) {
+    // Condition for isRequired
+    if(!(propName in props)) {
+      return new Error(`missing ${propName} in ${component} component`)
+    }
+    // Condition for type check
+    else if(typeof props[propName] !== 'string') {
+      return new Error(`Expected ${propName} to have type "string" instead of "${typeof props[propName]}" in ${component} component`)
+    }
+  }
+}
 
 export default App;
